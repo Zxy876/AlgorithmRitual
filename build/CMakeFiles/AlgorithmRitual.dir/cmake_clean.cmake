6d2a69ae1f1603bd2baf3048cb8e2fb6689567b1
Zxy876 <xiangyu_0514@qq.com>
2025-11-05 13:21:48 +0800

    配置环境

---
+++ build/CMakeFiles/AlgorithmRitual.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AlgorithmRitual"
+  "AlgorithmRitual.pdb"
+  "CMakeFiles/AlgorithmRitual.dir/src/main.cpp.o"
+  "CMakeFiles/AlgorithmRitual.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgorithmRitual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
